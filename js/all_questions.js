de05abb4d91fb6c583e37ac847a0e2dc2e5250b2
Michael Ruane <mruane@dvrpc.org>
2017-10-12 10:25:57 -0400

    Initial build

---
+++ js/all_questions.js
@@ -0,0 +1,12 @@
+// This is the file that contains all the question, answer and point value data.
+
+var json = [{
+"question": "Some random questions goes in here", 
+"answers": ["Games", "Sports or fitness equipment", "More meeting rooms", "Sleeping space", "Lounge", "Privacy", "Bar", "Playground"],
+"points": [21, 17, 16, 16, 8, 8, 7, 7]
+},
+{
+"question": "Another random question goes in here", 
+"answers": ["Hotel/hotel bar", "Somewhere with a view", "Somewhere warm", "On a boat", "Museum or park space", "Somewhere cheap", "Brewery", "Somewhere in Europe"],
+"points": [19, 19, 17, 15, 10, 8, 6, 6]
+}];
