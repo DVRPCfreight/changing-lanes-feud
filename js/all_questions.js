b10ee299f0664fa735a63bda34a3525e86bef98b
DVRPCfreight <mruane@dvrpc.org>
2017-10-16 16:28:51 -0400

    Draft improvements

--- js/all_questions.js
+++ js/all_questions.js
@@ -1,12 +1,22 @@
 // This is the file that contains all the question, answer and point value data.
 
 var json = [{
-"question": "Some random questions goes in here", 
-"answers": ["Games", "Sports or fitness equipment", "More meeting rooms", "Sleeping space", "Lounge", "Privacy", "Bar", "Playground"],
-"points": [21, 17, 16, 16, 8, 8, 7, 7]
+"question": "What mode of transportation would you be? ", 
+"answers": ["Plane", "Train", "Bicycle", "Boat", "Car", "Pedestrian", "Hoverboard", "Monorail"],
+"points": [32,24,12,9,8,7,2,1]
 },
 {
-"question": "Another random question goes in here", 
-"answers": ["Hotel/hotel bar", "Somewhere with a view", "Somewhere warm", "On a boat", "Museum or park space", "Somewhere cheap", "Brewery", "Somewhere in Europe"],
-"points": [19, 19, 17, 15, 10, 8, 6, 6]
-}];
+"question": "I knew I had made it when...", 
+"answers": ["Designated parking spot", "Journey is the adventure", "Stopped being called \"honey\"", "Not sure I've \"Made it\" yet ", "Won WTS Philly WOTY", "Became 1st woman Director", "White House called", "Got the Corner Office"],
+"points": ["JC","RS", "RC", "CC", "LR", "MP", "DH", "DB"]
+},
+{
+"question": "If I was not doing this job, I would be doing... ", 
+"answers": ["Professor", "Traveling and volunteering", "Working for MLB", "Not sure", "Audition for \"Amazing Race\"", "Time with grandchildren", "haven't thought about that", "steam for girls in schools"],
+"points": ["JC","RS", "RC", "CC", "LR", "MP", "DH", "DB"]
+},
+{
+"question": "Have you mastered the art of saying \"NO\"?", 
+"answers": ["No", "Yes", "working on it!", "is this a trick question?"],
+"points": [44,30,25,1]
+},];
